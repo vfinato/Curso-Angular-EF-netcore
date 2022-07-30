2c8cd823611da56f07d0024d7310ceaeef02dbb6
vfinato <vitor.finato@outlook.com>
2022-07-30 00:38:41 -0300

    alteracoes de projeto e plugins

---
+++ ProEventos/Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        { 
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "exemplo de get";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "exemplo de post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"exemplo de put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Post(int id)
+        {
+            return $"exemplo de delete com id = {id}";
+        }
+    }
+}
