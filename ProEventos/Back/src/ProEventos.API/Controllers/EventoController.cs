e4d7e210d9310c473556b55dc602cdebf7bc5adf
vfinato <vitor.finato@outlook.com>
2022-07-30 01:00:28 -0300

    criando rotas overload e estrutura basica

--- ProEventos/Back/src/ProEventos.API/Controllers/EventoController.cs
+++ ProEventos/Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -1,24 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
         public EventoController()
         { 
         }
 
+        private IEnumerable<Evento> _eventos = new Evento[]{
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular 11 e .net 5",
+                    Local = "Barra Bonita",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto.png"
+                },
+                new Evento(){
+                    EventoId = 2,
+                    Tema = "Angular e outros",
+                    Local = "Barra Bonita",
+                    Lote = "2º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto1.png"
+                }
+        };
+
         [HttpGet]
-        public string Get()
+        public IEnumerable<Evento> Get()
         {
-            return "exemplo de get";
+
+            return _eventos;
+            
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+
+            return _eventos.Where(e => e.EventoId == id);
+            
         }
 
         [HttpPost]
